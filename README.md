abf767d4f2edfad2104cb941a0c5700c99062364
mocnii <sestakm@yahoo.com>
2015-08-13 10:30:40 +0200

    initial commit - setting up CollectGenomes distribution - with noCPAN upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+CollectGenomes - It's new $module
+
+# SYNOPSIS
+
+    use CollectGenomes;
+
+# DESCRIPTION
+
+CollectGenomes is ...
+
+# LICENSE
+
+Copyright (C) mocnii.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mocnii <sestakm@yahoo.com>
---
+++ lib/CollectGenomes.pm
@@ -0,0 +1,39 @@
+package CollectGenomes;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+CollectGenomes - It's new $module
+
+=head1 SYNOPSIS
+
+    use CollectGenomes;
+
+=head1 DESCRIPTION
+
+CollectGenomes is ...
+
+=head1 LICENSE
+
+Copyright (C) mocnii.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mocnii E<lt>sestakm@yahoo.comE<gt>
+
+=cut
+
